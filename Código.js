c3c2a4e899d76063803958852dfa2a16bfb81281
Anthony Ramos <ramosm0106@gmail.com>
2024-08-29 18:15:21 -0300

    primer commit

---
+++ Código.js
@@ -0,0 +1,11 @@
+const doGet = () =>
+    HtmlService.createTemplateFromFile("frontend/index")
+        .evaluate()
+        .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+        .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+        .addMetaTag("viewport", 'width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1"')
+        .setTitle("Proyecto New")
+        //.setFaviconUrl("")
+        ;
+const include = (ruta) =>
+    HtmlService.createHtmlOutputFromFile(ruta).getContent();
