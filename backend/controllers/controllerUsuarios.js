4158eea7b1e1b2ef033cf3c58ff240f9b326e8b5
Anthony Ramos <ramosm0106@gmail.com>
2024-09-01 18:33:23 -0300

    comit con delete

--- backend/controllers/controllerUsuarios.js
+++ backend/controllers/controllerUsuarios.js
@@ -1,9 +1,9 @@
 function guardarUsuario(usuario) {
     try {
         //const {id, nombreCompleto, correo, contrasenia} = usuario;
-        const shetUsuarios= obtenerSheet(env_().SH_REGISTROS_USUARIOS);
+        const sheetUsuarios= obtenerSheet(env_().SH_REGISTROS_USUARIOS);
 
-        Insert(JSON.parse(usuario), shetUsuarios);
+        Insert(JSON.parse(usuario), sheetUsuarios);
         //shetUsuarios.appendRow([id, nombreCompleto, correo, contrasenia]);
         return{
             titulo: "Registro exitoso",
@@ -23,4 +23,38 @@ function listarUsuarios(id = undefined) {
    //return obtenerDatos(env_().SH_REGISTROS_USUARIOS);
    return JSON.stringify(_read(obtenerSheet(env_().SH_REGISTROS_USUARIOS), id));
     
+}
+
+
+function actualizarUsuario(id,datos){
+    
+    try {
+        //const {id,nombreCompleto,correo,password}= usuario; //desestructuración de los datos por parametro
+        const sheetUsuarios= obtenerSheet(env_().SH_REGISTROS_USUARIOS);
+        //sheetUsuarios.appendRow([id, nombreCompleto, correo, password]); //Se setean los datos a la shet
+        Update(id,datos,sheetUsuarios);
+        return {
+            titulo: "Actualizado Correctamente",
+            descripcion : "Ya se encuentra actualizado en la Base de datos"
+        };
+        
+    } catch (error) {
+        return{
+            titulo : "Ops ha ocurrido un error " + error,
+            descripcion:  "Contacte a soporte "
+        };
+    }
+}
+
+function eliminiarUsuario(id){
+    const sheetUsuarios= obtenerSheet(env_().SH_REGISTROS_USUARIOS);
+
+    const usuarioAEliminar= _read(sheetUsuarios, id);
+    sheetUsuarios.deleteRow(usuarioAEliminar.row);
+
+    return{
+        titulo : "Usuario Eliminado " ,
+        descripcion:  "Removido con exito"
+    };
+
 }
--- backend/helpers/readToJson.js
+++ backend/helpers/readToJson.js
@@ -1,30 +1,26 @@
+/**
+ * _read
+ * @param {String} sheet tabla de la base de datos
+ * @param {String} id un identificador para buscar 
+ * @returns {Object} objeto con la información necesaria
+ */
 function _read(sheet, id) {
     const data = sheet.getDataRange().getValues();
     const header = data.shift();
-    // Buscar todo
-    const resultado = data.map((row, indx) =>{
-        const reduced= header.reduce((accumulator,currentValue,currentIndex) => {
-            accumulator[currentValue] = row [currentIndex];
-            return accumulator;
-        },
-        {});
 
+    // Buscar todo
+    const resultado = data.map((row, indx) => {
+        const reduced = header.reduce((accumulator, currentValue, currentIndex) => {
+            accumulator[currentValue] = row[currentIndex];
+            return accumulator;
+        }, {});
         reduced.row = indx + 2;
         return reduced;
-    
-});
+    });
 
-// Filtrar por Id
-    if(id){
-        return resultado.find((dato) => dato.id === id);
-        //const datoFiltrado = resultado.find((dato) => {
-        //    if(dato.id === id){
-        //        return true;
-        //    }else{
-        //        return false;
-        //    }
-        //});
-        //return datoFiltrado;
+    // Filtrar si se pasa un ID
+    if (id) {
+        return datoFiltrado = resultado.find((dato) => dato.id === id);
     }
     return resultado;
 
